9464470583cd79b11623779605a9d111083f39cb
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2021-03-11 13:39:37 +0800

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 KATAGO_BACKEND=$1
-WEIGHT_FILE=$2
-RELEASE_VERSION=1.4.1
+WEIGHT_FILE="40b"
 GPU_NAME=`nvidia-smi -q | grep "Product Name" | cut -d":" -f2 | tr -cd '[:alnum:]._-'`
 #GPU_NAME=TeslaT4
 
@@ -21,27 +20,12 @@ detect_auto_backend () {
   fi
 }
 
-detect_auto_weight () {
-  if [ "$GPU_NAME" == "TeslaK80" ]
-  then
-    WEIGHT_FILE="20b"
-  elif [ "$GPU_NAME" == "TeslaP4" ]
-  then
-    WEIGHT_FILE="20b"
-  else
-    WEIGHT_FILE="40b"
-  fi
-}
 
 if [ "$KATAGO_BACKEND" == "AUTO" ]
 then
   detect_auto_backend
 fi
 
-if [ "$WEIGHT_FILE" == "AUTO" ]
-then
-  detect_auto_weight
-fi
 
 echo "Using GPU: " $GPU_NAME
 echo "Using Katago Backend: " $KATAGO_BACKEND
@@ -60,38 +44,26 @@ cmake /content/libzip-1.5.2/
 make && make install
 ln -s /usr/local/lib/libzip.so.5 /usr/lib
 
-cd /content
-
-if [ ! -d work ]
-then
-    wget  https://github.com/kinfkong/ikatago-for-colab/releases/download/$RELEASE_VERSION/work.zip -O work.zip
-    unzip -qq work.zip
-fi
-
 cd /content/work
-WEIGHT_URL=`grep "^$WEIGHT_FILE " ./weight_urls.txt | cut -d ' ' -f2`
+WEIGHT_URL="https://media.katagotraining.org/uploaded/networks/models/kata1/kata1-b40c256-s6809346304-d1651897329.bin.gz"
 echo "Using Weight URL: " $WEIGHT_URL
 
 #download the binarires
 if [ "$KATAGO_BACKEND" == "CUDA" ]
 then
-wget  https://github.com/lightvector/KataGo/releases/download/v1.7.0/katago-v1.7.0-gpu-cuda10.2-linux-x64.zip -O katago1.7.zip
+wget  https://github.com/lightvector/KataGo/releases/download/v1.8.0/katago-v1.8.0-cuda11.1-linux-x64.zip -O katago.zip
 else
-wget  https://github.com/lightvector/KataGo/releases/download/v1.7.0/katago-v1.7.0-gpu-opencl-linux-x64.zip -O katago1.7.zip
+wget  https://github.com/lightvector/KataGo/releases/download/v1.8.0/katago-v1.8.0-opencl-linux-x64.zip -O katago.zip
 fi
 
-unzip -qq  katago1.7.zip
+unzip -qq  katago.zip
 mv katago ./data/bins/katago
 chmod +x ./data/bins/katago
 #wget --quiet https://github.com/kinfkong/ikatago-for-colab/releases/download/$RELEASE_VERSION/katago-$KATAGO_BACKEND -O ./data/bins/katago
 
-mkdir -p /root/.katago/
-cp -r ./opencltuning /root/.katago/
 
 #download the weights
 wget --quiet $WEIGHT_URL -O "./data/weights/"$WEIGHT_FILE".bin.gz" 
 
-chmod +x ./change-config.sh
-./change-config.sh $WEIGHT_FILE "./data/weights/"$WEIGHT_FILE".bin.gz"
 
-chmod +x ./ikatago-server
+
